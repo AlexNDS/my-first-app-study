8b87bb32bc0cdabd89077aa784bd893833d2a0e2
unknown <scshepin@yandex.ru>
2021-10-20 23:53:57 +0800

    great my first app

---
+++ app/src/main/java/studyAlex/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package studyAlex.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
